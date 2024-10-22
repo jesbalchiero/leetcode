bcb3385ad382f58c84c2e42dee8bbacc7e9e0fd2
Jean Sbalchiero <jeancarlosbalchiero@gmail.com>
2024-10-22 10:29:56 -0300

    feat/problems: two sum c# solution

---
+++ problems/1/cs/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new();
+
+        for (int i = 0 ; 1 < nums.Length; i++) {
+            int diff = target - nums[i];
+
+            if (map.ContainsKey(nums[i]))
+                return new int[] {map[nums[i]], i};
+
+            map.TryAdd(diff, i);
+        }
+
+        return null;
+    }
+}
